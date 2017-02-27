306bc7920aa96f983bf86eee457580c23166336f
Mahdi Javaheri <m.javaherisaber@gmail.com>
2017-02-27 08:00:00 +0100

    Add runnable batch file

---
+++ Run.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+echo ########### Http Web Server in JAVA #############
+echo Explanation of project ::
+echo this project is intended to demonstrate web server concetps via HTTP protocol
+echo $ root folder is an example website to test HTTP GET protocol
+echo #################################################
+echo make sure 12000 port number is excluded from firewall
+echo and Open this website on the browser :
+echo localhost:12000
+java -jar "HttpWebServer.jar"
+pause
